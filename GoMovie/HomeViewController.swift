6e46e728dc93f53c102b234ec67c94a96799533a
Iskandar Herputra Wahidiyat <iskandarherputra@gmail.com>
2021-05-25 22:18:02 +0700

    Basic setup.

---
+++ GoMovie/HomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  HomeViewController.swift
+//  GoMovie
+//
+//  Created by Iskandar Herputra Wahidiyat on 25/05/21.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupView()
+    }
+    
+    //MARK: - Setup
+    private func setupView() {
+        title = "Home Page"
+    }
+}
